0fa3beeba9de7caae88ed1adab5647d7c49a182a
ls <ryanadrig@gmail.com>
2023-02-28 18:51:40 -0800

    simple async then

---
+++ async_await_then.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+
+a1()async{
+  String ares = "a1call";
+  print(ares);
+  return ares;
+}
+
+a2()async{
+  String ares = "a2call";
+  print(ares);
+  return ares;
+}
+
+a3()async{
+  String ares = "a3call";
+  print(ares);
+  return ares;
+}
+
+void main()async{
+await a1().then((a1res)async{
+  print("a1 res ~ " + a1res);
+  await a2().then((a2res)async{
+      print("a2 res ~ " + a2res);
+    a3().then((a3res){
+      print("a3 res ~ " + a3res);
+    });
+  });
+
+});
+
+}
