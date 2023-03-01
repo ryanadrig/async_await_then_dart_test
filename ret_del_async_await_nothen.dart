e85afac85987973ba26b1158fab889283a9831ae
ls <ryanadrig@gmail.com>
2023-02-28 19:40:19 -0800

    nothen example

---
+++ ret_del_async_await_nothen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+
+a1()async{
+  String ares = "a1call";
+  print(ares);
+  return ares;
+}
+
+a2()async{
+  String ares = "a2init";
+  print(ares);
+  var aresp = await Future.delayed(Duration(seconds:1), (){
+    ares = "a2call";
+    return ares;
+  });
+  return aresp;
+}
+
+a3()async{
+  String ares = "a3call";
+  print(ares);
+  return ares;
+}
+
+void main()async{
+var a1res =  await a1();
+  print("a1 res ~ " + a1res);
+
+  // returns string properly
+var a2res =  await a2();
+print("a2 res ~ " + a2res);
+
+var a3res =  await a3();
+print("a3 res ~ " + a3res);
+}
