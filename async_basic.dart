725db20f9de0bb2db6462449083de6a4c71eb37c
ls <ryanadrig@gmail.com>
2023-03-03 15:20:18 -0800

    async_basic rename and not both cases

--- async_basic.dart
+++ async_basic.dart
@@ -1,30 +1,58 @@
 import 'dart:async';
 
- Future<int> someFunction()  async {
+// compiler does not treat this as future, waits for value to return before continuing execution
+ Future<int> expensive()  async {
   int count = 0;
-  // return Future(() {
     for (int i = 0; i < 1000000000; i ++) {
       count += i;
     }
     print("done");
     return count;
-  // });
 }
 
-test2() async {
+sync_tasks() async {
   print("begin");
-  var a = await someFunction();
-  print('end');
+  var a = expensive();
+    print('end');
 }
 
-void _incrementCounter()  {
+void sync_tasks_caller()  {
   print("above");
-  test2();
+  sync_tasks();
   print("below");
 }
 
+
+Future<int> expensive2()  async {
+  int count = 0;
+  return Future(() {
+    for (int i = 0; i < 1000000000; i ++) {
+      count += i;
+    }
+    print("done");
+    return count;
+  });
+}
+
+sync_tasks2() async {
+  print("begin");
+  var a = await expensive2();
+  print('end');
+}
+
+void sync_tasks_caller2()  {
+  print("above");
+  sync_tasks2();
+  print("below");
+}
+
+
+
+
 void main()  {
 
-  _incrementCounter();
+  sync_tasks_caller();
+
+  sync_tasks_caller2();
 
 }
